4c12f59477e80b2fdea21c81cb32aab436c5746c
Brian Winkers <bwinkers@gmail.com>
2017-11-23 09:28:19 -0800

    Support 'relative' schema paths

--- src/Loader/FileLoader.php
+++ src/Loader/FileLoader.php
@@ -27,6 +27,20 @@ final class FileLoader implements LoaderInterface
      */
     public function load($path)
     {
+        // Check if the file path is 'relative', i.e. starts with a dot.
+        if(substr($path,0,1) === '.') {
+            // If the path is 'relative' check for an defined AR_JSON_SCHEMA_DIR
+            $schemaDir = getenv('AR_JSON_SCHEMA_DIR');
+
+            // If root dir isn't set use the default relative path of './schema'
+            if(! $schemaDir) {
+                $schemaDir = getcwd() . '/schema';
+            }
+          
+            // Define the full path to pass to existing League Reference File Loader
+            $path = $schemaDir . ltrim($path, '.');
+        }
+        
         if (!file_exists($path)) {
             throw SchemaLoadingException::notFound($path);
         }
